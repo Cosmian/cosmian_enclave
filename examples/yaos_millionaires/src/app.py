2abb8575c3ebe8738e4535fd3cf09a32b569caac
grydz <grydz@pm.me>
2024-12-31 12:40:19 +0400

    Add: FastAPI instead of Flask in Yao's Millionaires example

--- examples/yaos_millionaires/src/app.py
+++ examples/yaos_millionaires/src/app.py
@@ -6,103 +6,87 @@ import os
 import struct
 from http import HTTPStatus
 from pathlib import Path
-from typing import Any, Optional
+from typing import List, Optional
 
 from cenclave_lib_crypto.seal_box import seal, unseal
-from flask import Flask, Response, jsonify, request
+from fastapi import FastAPI, Request, Response
+from pydantic import BaseModel
 
 import globs
 
-app = Flask(__name__)
+app = FastAPI()
 
 CONFIG = json.loads((Path(__file__).parent / "config.json").read_text(encoding="utf-8"))
 
 ENCLAVE_SK: bytes = Path(os.environ["ENCLAVE_SK_PATH"]).read_bytes()
 
 
+class PushItemReq(BaseModel):
+    """Item request from /push endpoint."""
+
+    pk: str
+    data: str
+
+
+class RichestItemReq(BaseModel):
+    """Item request from /richest endpoint."""
+
+    recipient_pk: str
+
+
+class ParticipantsItemResp(BaseModel):
+    """Item response from /participants endpoint."""
+
+    participants: List[str]
+
+
+class RichestItemResp(BaseModel):
+    """Item response from /richest endpoint."""
+
+    max: Optional[str]
+
+
 @app.get("/health")
-def health_check() -> Response:
+async def health_check(request: Request) -> Response:
     """Health check of the application."""
-    return Response(response="OK", status=HTTPStatus.OK)
+    if "tls" in request.scope["extensions"]:
+        client_cert = request.scope["extensions"]["tls"]["client_cert_chain"]
+        print(f"client_cert: {client_cert}")
+    return Response(status_code=HTTPStatus.OK)
 
 
-@app.post("/")
-def push() -> Response:
+@app.post("/push")
+def push(item: PushItemReq) -> Response:
     """Add a number to the pool."""
-    content: Optional[Any] = request.get_json(silent=True)
+    if item.pk not in CONFIG["participants"]:
+        return Response(status_code=HTTPStatus.UNAUTHORIZED)
 
-    if content is None or not isinstance(content, dict):
-        app.logger.error("TypeError with data: '%s'", content)
-        return Response(status=HTTPStatus.UNPROCESSABLE_ENTITY)
+    if item.pk in dict(globs.POOL):
+        return Response(status_code=HTTPStatus.CONFLICT)
 
-    data: Optional[Any] = content.get("data")
-    pk: Optional[str] = content.get("pk")
-
-    if data is None or not isinstance(data, dict):
-        app.logger.error("TypeError with data content: '%s' (%s)", data, type(data))
-        return Response(status=HTTPStatus.UNPROCESSABLE_ENTITY)
-
-    if pk is None or not isinstance(pk, str):
-        app.logger.error("TypeError with data content: '%s' (%s)", pk, type(pk))
-        return Response(status=HTTPStatus.UNPROCESSABLE_ENTITY)
-
-    if pk not in CONFIG["participants"]:
-        app.logger.error(
-            "The public key provided is not in the participants: '%s' (%s)",
-            pk,
-            CONFIG["participants"],
-        )
-        return Response(status=HTTPStatus.UNPROCESSABLE_ENTITY)
-
-    if pk in dict(globs.POOL):
-        app.logger.error("Public key already pushed data")
-        return Response(status=HTTPStatus.CONFLICT)
-
-    n: bytes = unseal(base64.b64decode(data["n"]), ENCLAVE_SK)
-
+    n: bytes = unseal(base64.b64decode(item.data), ENCLAVE_SK)
     deser_n, *_ = struct.unpack("<d", n)
-    globs.POOL.append((pk, deser_n))
+    globs.POOL.append((item.pk, deser_n))
 
-    app.logger.info("Successfully added (%s, %s)", deser_n, pk)
-    return Response(status=HTTPStatus.OK)
+    return Response(status_code=HTTPStatus.OK)
 
 
 @app.get("/participants")
-def participants() -> Response:
+def participants():
     """Get all the public keys of participants"""
-    return jsonify(CONFIG)
+    return ParticipantsItemResp(participants=CONFIG["participants"])
 
 
 @app.post("/richest")
-def richest():
-    """Get the current max in pool."""
+def richest(item: RichestItemReq):
+    """Get the richest in pool."""
     if len(globs.POOL) < 1:
-        app.logger.error("need more than 1 value to compute the max")
-        return {"max": None}
+        return RichestItemResp(max=None)
 
-    data: Optional[Any] = request.get_json(silent=True)
+    if item.recipient_pk not in CONFIG["participants"]:
+        return Response(status_code=HTTPStatus.UNPROCESSABLE_ENTITY)
 
-    if data is None or not isinstance(data, dict):
-        app.logger.error("TypeError with data: '%s'", data)
-        return Response(status=HTTPStatus.UNPROCESSABLE_ENTITY)
-
-    recipient_pk: Optional[str] = data.get("recipient_pk")
-
-    if recipient_pk is None or not isinstance(recipient_pk, str):
-        app.logger.error(
-            "TypeError with data content: '%s' (%s)", recipient_pk, type(recipient_pk)
-        )
-        return Response(status=HTTPStatus.UNPROCESSABLE_ENTITY)
-
-    if recipient_pk not in CONFIG["participants"]:
-        app.logger.error(
-            "The public key provided is not in the participants: '%s' (%s)",
-            recipient_pk,
-            CONFIG["participants"],
-        )
-        return Response(status=HTTPStatus.UNPROCESSABLE_ENTITY)
-
-    raw_recipient_pk: bytes = base64.b64decode(recipient_pk)
+    raw_recipient_pk: bytes = base64.b64decode(item.recipient_pk)
 
     (pk, _) = max(globs.POOL, key=lambda t: t[1])
 
@@ -110,14 +94,12 @@ def richest():
         seal(base64.b64decode(pk), raw_recipient_pk)
     ).decode("utf-8")
 
-    return jsonify({"max": encrypted_b64_result})
+    return RichestItemResp(max=encrypted_b64_result)
 
 
 @app.delete("/")
-def reset():
+def reset() -> Response:
     """Reset the current pool."""
     globs.POOL = []
 
-    app.logger.info("Reset successfully")
-
-    return Response(status=HTTPStatus.OK)
+    return Response(status_code=HTTPStatus.OK)
